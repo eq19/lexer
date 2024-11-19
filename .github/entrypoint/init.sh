0c281c9cb8aabdefb05df1f705cbf76ebb6541cf
eq19 <eq19@users.noreply.github.com>
2024-11-19 01:35:57 +0000

    update workflows

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -56,11 +56,9 @@ if [[ "${JOB_ID}" == "3" ]]; then
   rm -rf ${RUNNER_TEMP}/Sidebar.md && cp _Sidebar.md ${RUNNER_TEMP}/Sidebar.md
   sed -i 's/0. \[\[//g' ${RUNNER_TEMP}/Sidebar.md && sed -i 's/\]\]//g' ${RUNNER_TEMP}/Sidebar.md
 
-  cd /home/runner/_site
+  cd /home/runner/_site && cp -R ${RUNNER_TEMP}/gistdir/* .
   find . -iname '*.md' -print0 | sort -zn | xargs -0 -I '{}' front.sh '{}'
   find . -type d -name "${FOLDER}" -prune -exec sh -c 'cat ${RUNNER_TEMP}/README.md >> $1/README.md' sh {} \;
-  
-  cp -R ${RUNNER_TEMP}/gistdir/* .
 
 fi
 
