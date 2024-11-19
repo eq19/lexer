408dc538ab4b47b8914429ffea11cd571b13fb8c
eq19 <eq19@users.noreply.github.com>
2024-11-19 09:33:52 +0000

    update workflows

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -44,7 +44,8 @@ if [[ -z ${PASS} ]] || [[ "${PASS}" == "true" ]]; then
   echo 'REMOTE_REPO='${REMOTE_REPO} >> ${GITHUB_ENV}
 
   if [[ -f _config.yml ]]; then
-    mapfile -t SITE < <(yq '.' _config.yml) && echo ${SITE[@]}
+    [[ "$RUNNER_OS" == "Windows" ]] && YQ=$(choco install yq)
+    mapfile -t site < <(yq '.' _config.yml) && echo ${site[@]}
   elif [[ -f /home/runner/_site/.env ]]; then
     set -a && . /home/runner/_site/.env && set +a
   fi
@@ -55,7 +56,7 @@ fi
 if [[ "${JOB_ID}" == "3" ]]; then
 
   echo -e "\n$hr\nWORKSPACE\n$hr"
-  gist.sh ${SITE["repository"]} ${SITE["span"]}
+  gist.sh ${{ site.repository }} ${{ site.span }}
   find ${RUNNER_TEMP}/gistdir -type d -name .git -prune -exec rm -rf {} \;
   
   mv -f ${RUNNER_TEMP}/workdir/* /home/runner/_site/
@@ -64,7 +65,7 @@ if [[ "${JOB_ID}" == "3" ]]; then
 
   cd /home/runner/_site && cp -R ${RUNNER_TEMP}/gistdir/* .
   find . -iname '*.md' -print0 | sort -zn | xargs -0 -I '{}' front.sh '{}'
-  find . -type d -name '${SITE["span"]' -prune -exec sh -c 'cat ${RUNNER_TEMP}/README.md >> $1/README.md' sh {} \;
+  find . -type d -name '${{ site.span }}' -prune -exec sh -c 'cat ${RUNNER_TEMP}/README.md >> $1/README.md' sh {} \;
 
 fi
 
