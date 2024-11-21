3883edbec58e5c494aace77902ac4e6d080fa0b8
eQuantum <8466209+eq19@users.noreply.github.com>
2024-11-21 23:32:16 +0700

    Update moxygen.js
    Signed-off-by: eQuantum <8466209+eq19@users.noreply.github.com>

--- bin/moxygen.js
+++ bin/moxygen.js
@@ -4,7 +4,7 @@
 var logger = require('../src/logger');
 //var program = require('commander');
 var assign = require('object-assign');
-var pjson = require('../src/package.json');
+var pjson = require('../package.json');
 var app = require('../index.js');
 
 import { program } from 'commander'; 
