6870a4cf4164902b9f33505b61d16e03101ee371
eQuantum <8466209+eq19@users.noreply.github.com>
2024-11-22 02:04:06 +0700

    Update moxygen.js
    Signed-off-by: eQuantum <8466209+eq19@users.noreply.github.com>

--- bin/moxygen.js
+++ bin/moxygen.js
@@ -1,28 +1,29 @@
 #!/usr/bin/env node
 'use strict';
 
-var logger = require('../src/logger');
+var log = require('winston');
 var program = require('commander');
 var assign = require('object-assign');
 var pjson = require('../package.json');
 var app = require('../index.js');
 
 program.version(pjson.version)
-  .usage('[options] <doxygen XML directory>')
-  .option('-o, --output <file>', 'output file, must contain "%s" when using `groups` or `classes` (default: "api.md"/"api_%s.md")', String)
+  .usage('[options] <doxygen directory>')
+  .option('-o, --output <file>', 'output file (must contain %s when using groups)', String, 'api.md')
   .option('-g, --groups', 'output doxygen groups into separate files', false)
-  .option('-c, --classes', 'output doxygen classes into separate files', false)
+  .option('-c, --classes', 'output doxygen groups into separate files', false)
   .option('-p, --pages', 'output doxygen pages into separate files', false)
-  .option('-n, --noindex', 'disable generation of the index, ignored with `groups` or `classes`', false)
+  .option('-n, --noindex', 'disable generation of the index (no effect with `groups` option', false)
   .option('-a, --anchors', 'add anchors to internal links', false)
-  .option('-H, --html-anchors', 'add html anchors to internal links', false)
+  .option('-h, --html-anchors', 'add html anchors to internal links', false)
   .option('-l, --language <lang>', 'programming language', String, 'cpp')
-  .option('-t, --templates <dir>', 'custom templates directory (default: "built-in templates")', String)
-  .option('-L, --logfile [file]', 'output log messages to file, (default: console only, default file name: "moxygen.log")')
+  .option('-t, --templates <dir>', 'custom templates directory', String, 'templates')
   .option('-q, --quiet', 'quiet mode', false)
   .parse(process.argv);
 
-logger.init(program, app.defaultOptions);
+if (!program.quiet) {
+  log.level = 'verbose';
+}
 
 if (program.args.length) {
   app.run(assign({}, app.defaultOptions, {
@@ -35,7 +36,7 @@ if (program.args.length) {
     anchors: program.anchors,
     htmlAnchors: program.htmlAnchors,
     language: program.language,
-    templates: program.templates,
+    templates: program.templates
   }));
 }
 else {
