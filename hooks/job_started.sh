300bbf50bc57feffe7c4a3bdf2993eac281a7439
eQuantum <8466209+eq19@users.noreply.github.com>
2025-01-01 20:53:59 +0700

    Update job_started.sh
    Signed-off-by: eQuantum <8466209+eq19@users.noreply.github.com>

--- hooks/job_started.sh
+++ hooks/job_started.sh
@@ -26,9 +26,9 @@ ls -al /etc/systemd
 
 echo -e "\n$hr\nRunner 🏠 dir\n$hr"
 ls -al /home/runner
-#cloud-init devel schema --config-file your-cloud-config.yml
 
 echo -e "\n$hr\nCloud-config Logs\n$hr"
+journalctl -u cloud-init
 echo -e "\n$hr\n"
 grep -i error /var/log/cloud-init.log
 cat /var/log/cloud-init.log
