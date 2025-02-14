2ceb5c83acb385fcf4bdf6df1e90174149dd73ad
eQuantum <8466209+eq19@users.noreply.github.com>
2025-02-14 16:06:18 +0700

    Update job_completed.sh
    Signed-off-by: eQuantum <8466209+eq19@users.noreply.github.com>

--- hooks/job_completed.sh
+++ hooks/job_completed.sh
@@ -16,7 +16,7 @@ if [ -d /mnt/disks/deeplearning/usr/local/sbin ]; then
 
   echo -e "\n$hr\nStart Network\n$hr"
   /mnt/disks/deeplearning/usr/bin/docker exec mydb supervisorctl start freqtrade
-  /mnt/disks/deeplearning/usr/bin/docker exec mydb supervisorctl start monitor_freqtrade
+  #/mnt/disks/deeplearning/usr/bin/docker exec mydb supervisorctl start monitor_freqtrade
 
 fi
 
