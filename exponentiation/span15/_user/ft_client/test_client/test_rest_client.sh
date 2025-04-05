8cece12d803e8fd0ae30185e5aa93d21cbcb0367
eq19 <eq19@users.noreply.github.com>
2025-04-05 22:23:04 +0000

    Update test_rest_client.sh

--- exponentiation/span15/_user/ft_client/test_client/test_rest_client.sh
+++ exponentiation/span15/_user/ft_client/test_client/test_rest_client.sh
@@ -193,14 +193,13 @@ else
   echo "NEW SCORE: $NEW_SCORE"
 
   if (( $(echo "$NEW_SCORE > $OLD_SCORE" | bc -l) )); then
-    cat $STRATEGY && cat $GITHUB_ENV
-    curl -L \
-      -X PUT \
+    cat $STRATEGY
+    curl -L -s -X PATCH \
       -H "Accept: application/vnd.github+json" \
       -H "Authorization: Bearer $GH_TOKEN" \
       -H "X-GitHub-Api-Version: 2022-11-28" \
       "https://api.github.com/repos/$TARGET_REPOSITORY/actions/variables/PARAMS_JSON" \
-      -d "{\"name\":\"PARAMS_JSON\",\"value\":\"$(jq -r '.' $STRATEGY)\"}"
+      -d "$(jq -n '{name:"PARAMS_JSON", value:$value}' --arg value "$(cat "$STRATEGY")")"
   fi
 
   #echo -e "\n$hr\nANALYSIS\n$hr"
